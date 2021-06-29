ea6ebcba25a1e25836c7530019b7aeb4e9711e5e
Isha Agarwal <isha.a1023@gmail.com>
2021-06-29 15:14:33 +0530

    ViewProfile added and navbar changed

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,13 +2,15 @@ import { useHistory } from "react-router-dom";
 import classes from "./Navbar.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch, faCog } from "@fortawesome/free-solid-svg-icons";
+import {NavLink} from 'react-router-dom'
+import Dropdown from "react-bootstrap/Dropdown";
 
 const Navbar = () => {
   let history = useHistory();
 
   const searchResult = (e) => {
-    console.log(e.target.value)
-  }
+    console.log(e.target.value);
+  };
 
   return (
     <nav className={"navbar " + classes.Navbar}>
@@ -23,19 +25,36 @@ const Navbar = () => {
         Recrutio
       </a>
       <div className={classes.Searchbar}>
-        <div className={classes.Search} >
+        <div className={classes.Search}>
           <FontAwesomeIcon
             icon={faSearch}
             style={{
               fontSize: "1.5em",
               padding: "1.2px",
-              color: 'gray'
+              color: "gray",
             }}
           />
-          <input placeholder="Search for the users with skills ex: Python " className={classes.SearchInput} />
+          <input
+            placeholder="Search for the users with skills ex: Python "
+            className={classes.SearchInput}
+          />
         </div>
       </div>
-      <FontAwesomeIcon icon={faCog} style={{color: 'white', fontSize: "1.5em"}} className={classes.Settings} />
+      <Dropdown>
+        <Dropdown.Toggle style={{backgroundColor: 'transparent', border: 'none'}} id="dropdown-basic">
+        <FontAwesomeIcon
+        icon={faCog}
+        style={{ color: "white", fontSize: "1.5em" }}
+        className={classes.Settings}
+      />
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item ><NavLink to="/profile">Your Profile</NavLink></Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
     </nav>
   );
 };
--- src/UI/ViewProfile/ViewProfile.js
+++ src/UI/ViewProfile/ViewProfile.js
@@ -1,30 +1,87 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Navbar from "../../Components/Navbar/Navbar";
-import classes from '../../Components/Profile/Profile.module.css'
+import classes from "../../Components/Profile/Profile.module.css";
 import ProfileCard from "../../Components/ProfileCard/ProfileCard";
+import DetailCard from "../DetailCard/DetailCard";
 
 const ViewProfile = (props) => {
+  const location = useLocation();
+  const [user, setUser] = useState("");
+  const [details, setDetails] = useState({});
 
-    const location = useLocation();
-    const [user, setUser] = useState('')
-    const [details, setDetails] = useState({})
+  useEffect(() => {
+    if (location.state) {
+      setUser(location.state.user);
+      setDetails(location.state.data);
+    }
+  }, [location]);
 
-    useEffect(() => {
-        if(location.state) {
-            setUser(location.state.user)
-            setDetails(location.state.data)
-        }
-    }, [location])
+  return (
+    <div style={{ backgroundColor: "#f3f3f3", minHeight: "100vh" }}>
+      <Navbar />
+      <div className={classes.MainDiv}>
+        <ProfileCard details={details} user={user} />
 
-    return(
-        <div style={{ backgroundColor: "#f3f3f3", minHeight: "100vh" }}>
-            <Navbar />
-            <div  className={classes.MainDiv} >
-                <ProfileCard details={details} user={user} />
+        <div className={classes.ProfileDiv}>
+          <div className={classes.Field}>
+            <div>
+              <div className={classes.FieldHead}>
+                <h3>Experience</h3>
+              </div>
+              {details.experience
+                ? details.experience
+                    .sort((a, b) => new Date(b.from) - new Date(a.from))
+                    .map((details, index) => (
+                      <DetailCard
+                        details={details}
+                        key={index}
+                        type="experience"
+                        edit={false}
+                      />
+                    ))
+                : ""}
             </div>
+          </div>
+          <div className={classes.Field}>
+            <div>
+              <div className={classes.FieldHead}>
+                <h3>Education</h3>
+              </div>
+              {details.education
+                ? details.education
+                    .sort((a, b) => new Date(b.from) - new Date(a.from))
+                    .map((details, index) => (
+                      <DetailCard
+                        details={details}
+                        key={index}
+                        type="education"
+                        edit={false}
+                      />
+                    ))
+                : ""}
+            </div>
+          </div>
+          <div className={classes.Field}>
+            <div>
+              <div className={classes.FieldHead}>
+                <h3>Skills</h3>
+              </div>
+              {details.skills
+                ? details.skills.map((skil, index) => {
+                    return (
+                      <button key={index} className={classes.Btn}>
+                        {skil} &nbsp;
+                      </button>
+                    );
+                  })
+                : ""}
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default ViewProfile
+export default ViewProfile;
